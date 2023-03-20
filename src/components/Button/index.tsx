bc64014855f2cd1d74907cd1262bd7fc14b71206
Vania <iv.ssh.fqu@gmail.com>
2023-03-20 13:34:33 +0200

    [add] button styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,28 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import styles from './styles.module.scss'
 
 interface ButtonProps {
     children: React.ReactNode
     onClick: () => void
+    variant?:  'primary' | 'secondary';
+    className?: string;
 }
-const Button:FC<ButtonProps> = ({children, onClick}) => {
+
+
+const Button:FC<ButtonProps> = ({children, onClick, variant, className}) => {
+    const [btnStyles, setBtnStyle] = useState('');
+
+    useEffect(() => {
+        setBtnStyle(
+            [
+                styles.button,
+                variant ? styles[variant] : styles.primary,
+                className ? className : '',
+            ].join(' ')
+        );
+    }, []);
     return (
-        <button onClick={onClick} className={styles.button}>
+        <button onClick={onClick} className={btnStyles}>
             {children}
         </button>
     );
