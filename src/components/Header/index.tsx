446a2ba4d5e2dfa40c524c106270fdb93398f86f
Gvich <wert130202@gmail.com>
2023-03-20 16:15:25 +0200

    style header (

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,23 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import Link from "next/link";
-import {Checkbox} from "@/components";
-import {StringContext} from "@/context/changeType";
-
+import {Button, Checkbox, TextInput} from "@/components";
+// import {StringContext} from "@/context/changeType";
+import styles from './styles.module.scss'
+import {useRouter} from "next/router";
 const Header = () => {
-    const {method} = useContext(StringContext);
+    // const {method} = useContext(StringContext);
+    const router = useRouter()
     return (
-        <div>
-            <div>
-                Logo
-                <h2>{method}</h2>
-            </div>
-            <div>
-                {/*Search/*/}
-                <div>
-                    <Link href={'/create-news'}>Create</Link>
-                </div>
-                <div>
-                    <Checkbox/>
+        <div className={styles.header}>
+            <div className={styles.group}>
+                <div><Link href={'/'} className={styles.news}>NEWS</Link></div>
+                <div className={styles.buttons}>
+                    <div><TextInput/></div>
+                    <div><Checkbox/></div>
+                    {/*<h2>{method}</h2>*/}
+                    <div>
+                        <Button onClick={()=> router.push('/create-news')}>Create</Button>
+                    </div>
                 </div>
             </div>
         </div>
